ad914561853dd54a9701e8fc4d9f6dedfc543936
tm512 <elykdav@gmail.com>
2016-07-14 11:57:43 -0700

    Add basic disassembler, clean up gen.c.

---
+++ src/disasm.c
@@ -0,0 +1,170 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "opcode.h"
+#include "vm.h"
+
+enum {
+	OA_8 = 1,
+	OA_16,
+	OA_32,
+	OA_DBL,
+	OA_STR
+};
+
+static struct tn_disasm_opinfo {
+	const char *name;
+	unsigned int opands[4];
+} opinfo[] = {
+	[OP_NOP] =	{ "NOP",	{ 0 } },
+	[OP_ADD] =	{ "ADD",	{ 0 } },
+	[OP_SUB] =	{ "SUB",	{ 0 } },
+	[OP_MUL] =	{ "MUL",	{ 0 } },
+	[OP_DIV] =	{ "DIV",	{ 0 } },
+	[OP_MOD] =	{ "MOD",	{ 0 } },
+	[OP_EQ] =	{ "EQ",		{ 0 } },
+	[OP_NEQ] =	{ "NEQ",	{ 0 } },
+	[OP_LT] =	{ "LT",		{ 0 } },
+	[OP_LTE] =	{ "LTE",	{ 0 } },
+	[OP_GT] =	{ "GT",		{ 0 } },
+	[OP_GTE] =	{ "GTE",	{ 0 } },
+	[OP_ANDL] =	{ "ANDL",	{ 0 } },
+	[OP_ORL] =	{ "ORL",	{ 0 } },
+	[OP_CAT] =	{ "CAT",	{ 0 } },
+	[OP_LCAT] =	{ "LCAT",	{ 0 } },
+	[OP_PSHI] =	{ "PSHI",	{ OA_32, 0 } },
+	[OP_PSHD] =	{ "PSHD",	{ OA_DBL, 0 } },
+	[OP_PSHS] =	{ "PSHS",	{ OA_STR, 0 } },
+	[OP_PSHV] =	{ "PSHV",	{ OA_16, OA_32, 0 } },
+	[OP_SET] =	{ "SET",	{ OA_32, 0 } },
+	[OP_POP] =	{ "POP",	{ OA_32, 0 } },
+	[OP_CLSR] =	{ "CLSR",	{ OA_16, 0 } },
+	[OP_SELF] =	{ "SELF",	{ 0 } },
+	[OP_NIL] =	{ "NIL",	{ 0 } },
+	[OP_GLOB] =	{ "GLOB",	{ OA_STR, 0 } },
+	[OP_ARGS] =	{ "ARGS",	{ OA_8, OA_32, 0 } },
+	[OP_JMP] =	{ "JMP",	{ OA_32, 0 } },
+	[OP_JNZ] =	{ "JNZ",	{ OA_32, 0 } },
+	[OP_JZ] =	{ "JZ",		{ OA_32, 0 } },
+	[OP_CALL] =	{ "CALL",	{ OA_32, 0 } },
+	[OP_TCAL] =	{ "TCAL",	{ OA_32, 0 } },
+	[OP_RET] =	{ "RET",	{ 0 } },
+	[OP_HEAD] =	{ "HEAD",	{ 0 } },
+	[OP_TAIL] =	{ "TAIL",	{ 0 } },
+	[OP_LSTS] =	{ "LSTS",	{ 0 } },
+	[OP_LSTE] =	{ "LSTE",	{ 0 } },
+	[OP_NEG] =	{ "NEG",	{ 0 } },
+	[OP_NOT] =	{ "NOT",	{ 0 } },
+	[OP_PRNT] =	{ "PRNT",	{ 0 } },
+	[OP_END] =	{ "END",	{ 0 } }
+};
+
+static inline uint8_t tn_disasm_read8 (struct tn_chunk *ch)
+{
+	return ch->code[ch->pc++];
+}
+
+static uint16_t tn_disasm_read16 (struct tn_chunk *ch)
+{
+	uint16_t i;
+
+	i = ch->code[ch->pc++];
+	i |= ch->code[ch->pc++] << 8;
+
+	return i;
+}
+
+static uint32_t tn_disasm_read32 (struct tn_chunk *ch)
+{
+	uint32_t i;
+
+	i = ch->code[ch->pc++];
+	i |= ch->code[ch->pc++] << 8;
+	i |= ch->code[ch->pc++] << 16;
+	i |= ch->code[ch->pc++] << 24;
+
+	return i;
+}
+
+static uint64_t tn_disasm_read64 (struct tn_chunk *ch)
+{
+	uint64_t i;
+
+	i = ch->code[ch->pc++];
+	i |= ch->code[ch->pc++] << 8;
+	i |= ch->code[ch->pc++] << 16;
+	i |= ch->code[ch->pc++] << 24;
+	i |= (uint64_t)ch->code[ch->pc++] << 32;
+	i |= (uint64_t)ch->code[ch->pc++] << 40;
+	i |= (uint64_t)ch->code[ch->pc++] << 48;
+	i |= (uint64_t)ch->code[ch->pc++] << 56;
+
+	return i;
+}
+
+static double tn_disasm_readdouble (struct tn_chunk *ch)
+{
+	uint64_t u64 = tn_disasm_read64 (ch);
+	double *d = (double*)&u64;
+	return *d;
+}
+
+static char *tn_disasm_readstring (struct tn_chunk *ch)
+{
+	uint16_t len;
+	char *ret;
+
+	len = tn_disasm_read16 (ch);
+	ret = strndup ((char*)ch->code + ch->pc, len);
+	if (!ret)
+		return NULL;
+
+	ch->pc += len;
+	return ret;
+}
+
+void tn_disasm (struct tn_chunk *ch)
+{
+	int i;
+	struct tn_disasm_opinfo *op;
+
+	printf ("chunk %x (%s):\n", ch, ch->name);
+
+	ch->pc = 0;
+
+	while (1) {
+		printf ("%.5x ", ch->pc);
+
+		op = &opinfo[ch->code[ch->pc++]];
+		printf ("%-5s", op->name);
+
+		for (i = 0; op->opands[i]; i++) {
+			switch (op->opands[i]) {
+				case OA_8:
+					printf ("0x%x ", tn_disasm_read8 (ch));
+					break;
+				case OA_16:
+					printf ("0x%x ", tn_disasm_read16 (ch));
+					break;
+				case OA_32:
+					printf ("0x%x ", tn_disasm_read32 (ch));
+					break;
+				case OA_DBL:
+					printf ("%g ", tn_disasm_readdouble (ch));
+					break;
+				case OA_STR:
+					printf ("%s ", tn_disasm_readstring (ch));
+					break;
+				default: break;
+			}
+		}
+
+		printf ("\n");
+		if (op == &opinfo[OP_RET])
+			break;
+	}
+
+	for (i = 0; i < ch->subch_num; i++)
+		tn_disasm (ch->subch[i]);
+}
--- src/gen.c
+++ src/gen.c
@@ -82,7 +82,6 @@ static void tn_gen_emitpos (struct tn_chunk *ch, uint32_t n, uint32_t pos)
 	ch->code[pos++] = (n & 0xff000000) >> 24;
 }
 
-#define printf(...) (0) // silence
 static void tn_gen_ident (struct tn_chunk *ch, const char *name)
 {
 	struct tn_chunk *it = ch;
@@ -94,7 +93,6 @@ static void tn_gen_ident (struct tn_chunk *ch, const char *name)
 			tn_gen_emit8 (ch, OP_PSHV);
 			tn_gen_emit16 (ch, frame); // how many frames up we need to go to find a binding
 			tn_gen_emit32 (ch, id);
-			printf ("PSHV %u %u (%s)\n", frame, id, name);
 			return;
 		}
 
@@ -108,7 +106,6 @@ static void tn_gen_ident (struct tn_chunk *ch, const char *name)
 	tn_gen_emitstring (ch, name);
 	tn_gen_emit8 (ch, OP_SET);
 	tn_gen_emit32 (ch, id);
-	printf ("GLOB %s\nSET  %s (%i)\n", name, name, id);
 }
 
 #define op(NAME) [TOK_##NAME] = OP_##NAME
@@ -154,17 +151,14 @@ static void tn_gen_expr (struct tn_chunk *ch, struct tn_expr *ex, int final)
 		case EXPR_INT:
 			tn_gen_emit8 (ch, OP_PSHI);
 			tn_gen_emit32 (ch, ex->data.i);
-			printf ("%.5s %i\n", "PSHI", ex->data.i);
 			break;
 		case EXPR_FLOAT:
 			tn_gen_emit8 (ch, OP_PSHD);
 			tn_gen_emitdouble (ch, ex->data.d);
-			printf ("%.5s %g\n", "PSHD", ex->data.d);
 			break;
 		case EXPR_STRING:
 			tn_gen_emit8 (ch, OP_PSHS);
 			tn_gen_emitstring (ch, ex->data.s);
-			printf ("PSHS \"%s\"\n", ex->data.s);
 			break;
 		case EXPR_IDENT:
 			tn_gen_ident (ch, ex->data.id);
@@ -175,20 +169,16 @@ static void tn_gen_expr (struct tn_chunk *ch, struct tn_expr *ex, int final)
 			tn_gen_expr (ch, ex->data.assn.expr, 0);
 			tn_gen_emit8 (ch, OP_SET);
 			tn_gen_emit32 (ch, id);
-			printf ("%.5s %s (%i)\n", "SET", ex->data.assn.name, id);
 			break;
 		case EXPR_FN: {
 			struct tn_chunk *new;
 
-			printf (".FN  %i\n", nfunc++);
 			new = tn_gen_compile (ex->data.fn.expr, &ex->data.fn, ch, NULL);
-			printf (".END %i\n", --nfunc);
 			array_add (ch->subch, new);
 			sub = ch->subch[ch->subch_num - 1];
 
 			tn_gen_emit8 (ch, OP_CLSR);
 			tn_gen_emit16 (ch, ch->subch_num - 1);
-			printf ("%.5s %i\n", "CLSR", ch->subch_num - 1);
 			break;
 		}
 		case EXPR_UOP:
@@ -204,42 +194,34 @@ static void tn_gen_expr (struct tn_chunk *ch, struct tn_expr *ex, int final)
 				tn_gen_emit8 (ch, ex->data.bop.op == TOK_ANDL ? OP_JZ : OP_JNZ);
 				j1 = ch->pc;
 				tn_gen_emit32 (ch, 0);
-				printf ("%s\n", ex->data.bop.op == TOK_ANDL ? "JZ   .FALSE" : "JNZ  .TRUE");
 
 				tn_gen_expr (ch, ex->data.bop.right, 0);
 				tn_gen_emit8 (ch, ex->data.bop.op == TOK_ANDL ? OP_JZ : OP_JNZ);
 				j2 = ch->pc;
 				tn_gen_emit32 (ch, 0);
-				printf ("%s\n", ex->data.bop.op == TOK_ANDL ? "JZ   .FALSE" : "JNZ  .TRUE");
 
 				// results
 				tn_gen_emit8 (ch, OP_PSHI);
 				tn_gen_emit32 (ch, ex->data.bop.op == TOK_ANDL);
-				printf ("PSHI %i\n", ex->data.bop.op == TOK_ANDL);
 				tn_gen_emit8 (ch, OP_JMP);
 				out = ch->pc;
 				tn_gen_emit32 (ch, 0);
-				printf ("JMP  .OUT\n");
 
 				// false/true
 				tn_gen_emitpos (ch, ch->pc, j1);
 				tn_gen_emitpos (ch, ch->pc, j2);
-				printf (".%s (%x)\n", ex->data.bop.op == TOK_ANDL ? "FALSE" : "TRUE", ch->pc);
 
 				tn_gen_emit8 (ch, OP_PSHI);
 				tn_gen_emit32 (ch, ex->data.bop.op == TOK_ORL);
-				printf ("PSHI %i\n", ex->data.bop.op == TOK_ORL);
 
 				// out
 				tn_gen_emitpos (ch, ch->pc, out);
-				printf (".OUT (%x)\n", ch->pc);
 			}
 			else {
 				tn_gen_expr (ch, ex->data.bop.left, 0);
 				tn_gen_expr (ch, ex->data.bop.right, 0);
 
 				tn_gen_emit8 (ch, bop_opcodes[ex->data.bop.op]);
-				printf ("%.5s\n", bops[bop_opcodes[ex->data.bop.op] - OP_ADD]);
 			}
 			break;
 		case EXPR_CALL: {
@@ -257,7 +239,6 @@ static void tn_gen_expr (struct tn_chunk *ch, struct tn_expr *ex, int final)
 			tn_gen_expr (ch, ex->data.call.fn, 0);
 			tn_gen_emit8 (ch, final ? OP_TCAL : OP_CALL);
 			tn_gen_emit32 (ch, nargs);
-			printf ("CALL %i\n", nargs);
 			break;
 		}
 		case EXPR_IF: {
@@ -267,18 +248,14 @@ static void tn_gen_expr (struct tn_chunk *ch, struct tn_expr *ex, int final)
 			tn_gen_emit8 (ch, OP_JZ);
 			tskip = ch->pc;
 			tn_gen_emit32 (ch, 0);
-			printf ("%.5s .FALSE\n", "JZ");
 
 			tn_gen_expr (ch, ex->data.ifs.t, final); // pass through "final" here
 			tn_gen_emit8 (ch, OP_JMP);
 			fskip = ch->pc;
 			tn_gen_emit32 (ch, 0);
-			printf ("%.5s .SKIP\n", "JMP");
-			printf (".FALSE (%x)\n", ch->pc);
 			tn_gen_emitpos (ch, ch->pc, tskip); // we jump here on false
 
 			tn_gen_expr (ch, ex->data.ifs.f, final);
-			printf (".SKIP (%x)\n", ch->pc);
 			tn_gen_emitpos (ch, ch->pc, fskip); // we jump here after true
 			break;
 		}
@@ -288,21 +265,16 @@ static void tn_gen_expr (struct tn_chunk *ch, struct tn_expr *ex, int final)
 			tn_gen_expr (ch, ex->data.accs.expr, 0);
 			if (ex->data.accs.item->type == EXPR_IDENT) {
 				item = ex->data.accs.item->data.s;
-				if (item[0] == 'h') {
+				if (item[0] == 'h')
 					tn_gen_emit8 (ch, OP_HEAD);
-					printf ("HEAD\n");
-				}
-				else if (item[0] == 't') {
+				else if (item[0] == 't')
 					tn_gen_emit8 (ch, OP_TAIL);
-					printf ("TAIL\n");
-				}
 			}
 			break;
 		}
 		case EXPR_PRNT:
 			tn_gen_expr (ch, ex->data.print, 0);
 			tn_gen_emit8 (ch, OP_PRNT);
-			printf ("PRNT\n");
 			break;
 		default: break;
 	}
@@ -332,7 +304,6 @@ struct tn_chunk *tn_gen_compile (struct tn_expr *ex, struct tn_expr_data_fn *fn,
 		tn_gen_emit8 (ret, OP_ARGS);
 		tn_gen_emit8 (ret, fn->varargs);
 		tn_gen_emit32 (ret, fn->args_num);
-		printf ("ARGS %u %i\n", fn->varargs, fn->args_num);
 	}
 
 	while (it) {
@@ -346,7 +317,5 @@ struct tn_chunk *tn_gen_compile (struct tn_expr *ex, struct tn_expr_data_fn *fn,
 	}
 
 	tn_gen_emit8 (ret, OP_RET);
-	printf ("RET\n");
-
 	return ret;
 }
